c3aa5ad1e167a8bb23e9a43649e94dc6349bcc1e
creatorgaming <divyanshutyagi8@gmail.com>
2022-01-22 12:15:36 +0530

    Styling updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,18 @@
 function Header() {
 	return (
-		<div className="grid gap-4 grid-cols-2">
-			<div className=" border-2 border-black grid grid-cols-2 gap-2">
+		<div className="grid grid-cols-[1fr_3fr] justify-items-end">
+			<div className="grid grid-cols-[1fr_5fr]">
 				<img
 					// className="border"
 					src="https://via.placeholder.com/150"
 					alt="Icon"
+					className="scale-[0.8]"
 				/>
-				<span className="text-4xl">Job Listing</span>
+				<span className="text-3xl self-center">Job Listing</span>
 			</div>
-			<div>
-				<span>Job Listing</span>
+			<div className="grid gap-4 grid-cols-2">
+				<button>Sign Up</button>
+				<button>Sign In</button>
 			</div>
 		</div>
 	);
